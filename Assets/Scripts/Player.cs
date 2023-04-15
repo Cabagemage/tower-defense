ed5881e93601aad761266af3ee6baa64aba52f29
Andrey <scootaloorainbow@yandex.ru>
2023-04-15 18:41:59 +0300

    feat: init project

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+ * https://vk.com/doc23314759_320922509?hash=PgiHvfNyx8QE7ruZjakaI8wSWz2mmwuTaY8CdIbcihw&dl=XtU3h95dxI12Uzd6P1H8ZZUcOSrS9blxRJSzxhdLf2H
+ */
+public class Player : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
